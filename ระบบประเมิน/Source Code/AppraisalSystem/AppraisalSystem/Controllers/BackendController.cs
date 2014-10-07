6b6f7c2f30a503701c07272e3fa2165f82a52679
SOFTIEZ <num.atcharee@gmail.com>
2014-10-08 01:11:17 +0700

    เพิ่มส่วน Admin
    เพิ่ม AdminController with css ยังมีตกไปบ้าง ค่อยแก้

---
+++ ระบบประเมิน/Source Code/AppraisalSystem/AppraisalSystem/Controllers/BackendController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppraisalSystem.Controllers
+{
+    public class BackendController : Controller
+    {
+        //
+        // GET: /Backend/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Manage()
+        {
+            return View();
+        }
+
+    }
+}
